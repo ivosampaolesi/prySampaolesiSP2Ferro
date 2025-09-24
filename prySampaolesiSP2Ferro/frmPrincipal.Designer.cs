fe100634a4e4a2c80f9f12cf41011d09bce2bdb3
Ivo Sampaolesi <ivodribe@gmail.com>
2025-09-23 22:23:08 -0300

    Icono Agregado

---
+++ prySampaolesiSP2Ferro/frmPrincipal.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace prySampaolesiSP2Ferro
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            maskedTextBox1 = new MaskedTextBox();
+            SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(121, 71);
+            maskedTextBox1.Mask = "00000km";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(219, 27);
+            maskedTextBox1.TabIndex = 0;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(503, 277);
+            Controls.Add(maskedTextBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frmPrincipal";
+            Text = "Calculadora de precio del boleto";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaskedTextBox maskedTextBox1;
+    }
+}
--- prySampaolesiSP2Ferro/frmPrincipal.cs
+++ prySampaolesiSP2Ferro/frmPrincipal.cs
@@ -1,8 +1,8 @@
 namespace prySampaolesiSP2Ferro
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
